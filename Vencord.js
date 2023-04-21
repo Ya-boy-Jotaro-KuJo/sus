7a18b973a4fd1f9d8afac30c56d611c3cfc1fe15
JioJio <116957464+Ya-boy-Jotaro-KuJo@users.noreply.github.com>
2023-04-21 10:18:16 +0300

    Update Vencord.js

--- Vencord.js
+++ Vencord.js
@@ -1,11 +1,11 @@
 // some shiit
 const script = document.createElement("script");
-script.src = browser.runtime.getURL("/chromium-unpacked/dist/vencord.js");
+script.src = browser.runtime.getURL("C:/Users/HP/AppData/Roaming/BetterDiscord/plugins/chromium-unpacked/dist/vencord.js");
 
 const style = document.createElement("link");
 style.type = "text/css";
 style.rel = "stylesheet";
-style.href = browser.runtime.getURL("chromium-unpacked/dist/Vencord.css");
+style.href = browser.runtime.getURL("C:/Users/HP/AppData/Roaming/BetterDiscord/plugins/chromium-unpacked/dist/vencord.js");
 
 document.documentElement.append(script);
 
