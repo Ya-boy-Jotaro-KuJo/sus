073b397d263e11b60cf95bf87516ae6763040d32
JioJio <116957464+Ya-boy-Jotaro-KuJo@users.noreply.github.com>
2023-04-23 03:04:26 +0300

    Update Vencord.js

--- Vencord.js
+++ Vencord.js
@@ -2,9 +2,3 @@
 const script = document.createElement("script");
 script.src = browser.runtime.getURL("C:/Users/HP/AppData/Roaming/BetterDiscord/plugins/chromium-unpacked/dist/vencord.js");
 document.documentElement.append(script);
-
-document.addEventListener(
-    "DOMContentLoaded",
-    () => document.documentElement.append(style),
-    { once: true }
-);
